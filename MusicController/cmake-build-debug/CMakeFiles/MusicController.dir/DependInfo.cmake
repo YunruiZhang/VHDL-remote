c70a7645bb46c6a23150ed2bd381bca4bdc9dedc
Charlie <hts.epic@gmail.com>
2020-10-10 19:38:58 +0800

    feat: made some basic functions

---
+++ MusicController/cmake-build-debug/CMakeFiles/MusicController.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/kpz/Downloads/GitHub/3601-Project/MusicController/controller/Controller.cpp" "/Users/kpz/Downloads/GitHub/3601-Project/MusicController/cmake-build-debug/CMakeFiles/MusicController.dir/controller/Controller.cpp.o"
+  "/Users/kpz/Downloads/GitHub/3601-Project/MusicController/controller/MusicList.cpp" "/Users/kpz/Downloads/GitHub/3601-Project/MusicController/cmake-build-debug/CMakeFiles/MusicController.dir/controller/MusicList.cpp.o"
+  "/Users/kpz/Downloads/GitHub/3601-Project/MusicController/main.cpp" "/Users/kpz/Downloads/GitHub/3601-Project/MusicController/cmake-build-debug/CMakeFiles/MusicController.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ MusicController/cmake-build-debug/CMakeFiles/MusicController.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MusicController.dir/controller/Controller.cpp.o"
+  "CMakeFiles/MusicController.dir/controller/MusicList.cpp.o"
+  "CMakeFiles/MusicController.dir/main.cpp.o"
+  "MusicController"
+  "MusicController.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MusicController.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
